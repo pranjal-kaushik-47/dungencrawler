f005835bfd48b2abb1cc0c2c985628c0e85d8660
pranjal-kaushik-47 <kaushikpranjal.47007@gmail.com>
2025-03-24 11:18:05 +0000

    code cleanup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
     origin: "0.0.0.0:5173",
-    hmr: {
-      host: '0.0.0.0', // Use the host machine domain
-      protocol: 'ws',  // Force WebSocket protocol
-    }
   },
   plugins: [
     vue(),
