d0b4472bf1d90cf70f45ac44cf0031e80db2b17f
pranjal-kaushik-47 <kaushikpranjal.47007@gmail.com>
2025-03-24 11:46:16 +0000

    first component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import HealthBar from './components/HealthBar.vue';
+
+const health = ref("100");
 </script>
 
 <template>
@@ -6,76 +10,7 @@
     <div class="game-description">
         <h1>Escape the Dungeon</h1>
     </div>
-    <div class="health" id="health">
-        <div class="healthbar" id="healthbar"></div>
-    </div>
-    <div class="interactions">
-        <div class="outcome">
-            <textarea disabled=true id="outcome-ta">outcome of your actions...</textarea>
-        </div>
-        <h2>Action</h2>
-        <div>
-            <div class="actions" id="all-actions">
-                <button id="action-spawn">Spawn Spell</button>
-            </div>
-        </div>
-        <h2>Movement</h2>
-        <div class="movements">
-            <button id="move-forward" disabled=true>forward</button>
-            <button id="move-backward" disabled=true>backward</button>
-            <button id="move-left" disabled=true>left</button>
-            <button id="move-right" disabled=true>right</button>
-        </div>
-    </div>
-    <div class="bottom-window">
-        <div class="inventory">
-            <h2>Inventory</h2>
-            <div class="items">
-                <table id="invTable">
-                    <thead>
-                        <tr>
-                            <th>Items</th>
-                            <th>Description</th>
-                            <th style="width: 100px;">Use</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="cover">
-        </div>
-    </div>
+    <HealthBar :health="health"></HealthBar>
+    
 </body>
 </template>
---
+++ frontend/src/components/HealthBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="health" id="health">
+        <div class="healthbar" id="healthbar" :style="{width: healthLeft + '%'}"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        health: {
+            type: String
+        }
+    },
+    computed: {
+        healthLeft() {
+            return this.health
+        }
+    }
+}
+</script>
