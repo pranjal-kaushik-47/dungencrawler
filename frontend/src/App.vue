59df7594806ffe3dfa32749f01e225317c61a82a
pranjal-kaushik-47 <kaushikpranjal.47007@gmail.com>
2025-03-24 12:13:05 +0000

    init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import HealthBar from './components/HealthBar.vue';
+import GameWindow from './components/GameWindow.vue';
 
 const health = ref("100");
 </script>
 
 <template>
 <body>
-    <div class="game-description">
+    <div class="flex-window topic">
         <h1>Escape the Dungeon</h1>
     </div>
     <HealthBar :health="health"></HealthBar>
-    
+    <GameWindow class="onethird">
+        <textarea class="fullscreen" disabled=true></textarea>
+    </GameWindow>
 </body>
 </template>
---
+++ frontend/src/components/GameWindow.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="game-window"><slot></slot>
+    </div>
+</template>
+<script>
+export default {}
+</script>
